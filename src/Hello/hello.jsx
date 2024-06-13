391817b7f4ec1d1a26b5f3235ca5794a51fd1fe6
y-saranya <yagateelasaranya@gmail.com>
2024-06-13 18:27:52 +0530

    added Morning

--- src/Hello/hello.jsx
+++ src/Hello/hello.jsx
@@ -3,7 +3,7 @@ import './style.css'
 
 function Hello() {
     return (
-        <div className='colour'>Hello! Good Evening..</div>
+        <div className='colour'>Hello! Good Morning..</div>
     )
 }
 
